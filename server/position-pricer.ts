7dea64c32dc1a7f15cf96f9ac1670169bb4b729f
Mehemt <mehmtboga@gmail.com>
2026-08-23 13:58:25 +0300

    Update position-pricer.ts

--- server/position-pricer.ts
+++ server/position-pricer.ts
@@ -116,7 +116,7 @@ export class PositionPricer {
     for (const pos of openPositions) {
       const priceData = data[pos.mintAddress];
 
-      const solPrice = this.solPriceUsd > 0 ? this.solPriceUsd : 68;
+      const solPrice = this.solPriceUsd > 0 ? this.solPriceUsd : 94;
       const currentPriceUsd = priceData?.usdPrice ?? 0;
       const priceInSol = currentPriceUsd / solPrice;
 
